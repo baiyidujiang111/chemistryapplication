f54783dacc733c75fa660e7c36c474c267b6388b
baiyidujiang111 <48401789+baiyidujiang111@users.noreply.github.com>
2020-04-20 08:02:57 +0800

    Add files via upload

--- chemistry_application_android/app/src/main/java/com/example/chemistryapplication/yuansu.java
+++ chemistry_application_android/app/src/main/java/com/example/chemistryapplication/yuansu.java
@@ -27,13 +27,9 @@ public class yuansu extends AppCompatActivity {
                 /*Data.data2=enm.getText().toString();
                 if(Data.data2.equals("na"))
                 {*/
-                    textView.setText("钠是一种金属元素，在周期表中位于第3周期、第ⅠA族，是碱金属元素的代表，质地柔软，能与水反应生成氢氧化钠，放出氢气，化学性质较活泼。钠元素以盐的形式广泛的分布于陆地和海洋中，钠也是人体肌肉组织和神经组织中的重要成分之一。" +
-                            "4Na + O2= 2Na2O （常温）\n" +
-                            "2Na+O2= Na2O2（加热或点燃）\n" +
-                            "2Na+2H2O=2NaOH+H2↑\n" +
-                            "2Na+2CO2 = Na2CO3+CO\n" +
-                            "2Na+2ROH=2RONa+H2↑ （ROH表示低元醇）\n" +
-                            "2Na + 2NH3（L）= 2NaNH2 + H2↑（此反应中“2NH3（L）”表示液氨）");
+                Data.name=enm.getText().toString();
+                Cominacation com=new Cominacation();
+                com.TestServlet();
                 //}
             }
         });
